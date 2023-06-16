e5a2c3b4deef3a8a8a3f46198d4ba8faddfdc608
agtTwilight <lukasmacmillen@gmail.com>
2023-06-16 13:33:51 -0700

    styled and made functional carousel for desktop

--- src/components/homeMain/index.js
+++ src/components/homeMain/index.js
@@ -1,12 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import left from './assets/left.png';
 import location from './assets/location.png';
+import right from './assets/right.png';
 import './style.css';
 
 export const HomeMain = () => {
-	const [slide, setSlide] = useState(1);
+	const [slide, setSlide] = useState(0);
 
-	const nextSlide = () => {};
-	const perviousSlide = () => {};
+	const nextSlide = () => {
+		if (slide !== 3) {
+			const carousel = document.querySelector(
+				'.snap-container-horizontal'
+			).children;
+
+			const nav = document.querySelector('#carousel-nav').children;
+
+			carousel[slide].classList.add('hide-carousel');
+			nav[slide].classList.remove('active-nav');
+			nav[slide + 1].classList.add('active-nav');
+
+			setSlide(slide + 1);
+		}
+	};
+
+	const perviousSlide = () => {
+		if (slide !== 0) {
+			const carousel = document.querySelector(
+				'.snap-container-horizontal'
+			).children;
+
+			const nav = document.querySelector('#carousel-nav').children;
+
+			carousel[slide - 1].classList.remove('hide-carousel');
+			nav[slide].classList.remove('active-nav');
+			nav[slide - 1].classList.add('active-nav');
+
+			setSlide(slide - 1);
+		}
+	};
+
+	const handleNavClick = (e) => {
+		if (!e.target.classList.length) {
+			const target = e.target.id.slice(-1) - 1;
+			const carousel = document.querySelector(
+				'.snap-container-horizontal'
+			).children;
+			const nav = document.querySelector('#carousel-nav').children;
+
+			if (target > slide) {
+				for (let i = 0; i < target; i++) {
+					carousel[i].classList.add('hide-carousel');
+					nav[i].classList.remove('active-nav');
+				}
+			} else {
+				for (let i = slide; i > target - 1; i--) {
+					carousel[i].classList.remove('hide-carousel');
+					nav[i].classList.remove('active-nav');
+				}
+			}
+
+			nav[target].classList.add('active-nav');
+			setSlide(target);
+		}
+	};
 
 	return (
 		<section id="home-main" className="snap-page-vertical">
@@ -21,7 +77,12 @@ export const HomeMain = () => {
 				></img>
 			</a>
 			<section id="carousel">
-				<button id="carousel-left" onClick={nextSlide}></button>
+				<img
+					id="carousel-left"
+					onClick={perviousSlide}
+					src={left}
+					alt="a left arrow"
+				></img>
 				<section className="snap-container-horizontal">
 					<div id="carousel-1" className="snap-page-horizontal">
 						<h1>About</h1>
@@ -35,16 +96,26 @@ export const HomeMain = () => {
 						<h1>Catering</h1>
 						<p>Schedule catering here</p>
 					</div>
-					<div className="snap-page-horizontal">
+					<div id="carousel-4" className="snap-page-horizontal">
 						<h1>Facebook</h1>
 						<p>Keep up with us on Facebook!</p>
 					</div>
 				</section>
-				<button id="carousel-right" onClick={perviousSlide}></button>
+				<img
+					id="carousel-right"
+					onClick={nextSlide}
+					src={right}
+					alt="a right arrow"
+				></img>
 				<div id="carousel-nav">
-					<button id="nav-1"></button>
-					<button id="nav-2"></button>
-					<button id="nav-3"></button>
+					<button
+						id="nav-1"
+						className="active-nav"
+						onClick={handleNavClick}
+					></button>
+					<button id="nav-2" onClick={handleNavClick}></button>
+					<button id="nav-3" onClick={handleNavClick}></button>
+					<button id="nav-4" onClick={handleNavClick}></button>
 				</div>
 			</section>
 			<div id="home-main-gradient"></div>
